97fb4206498fb7eb80649a999c0831c60437862d
Raunak917 <2300520400051@ietlucknow.ac.in>
2026-06-16 02:01:42 +0530

    retored map functionality

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,36 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict'
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// (() => {
+//   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//   const forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+//   // Loop over them and prevent submission
+//   Array.from(forms).forEach(form => {
+//     form.addEventListener('submit', event => {
+//       if (!form.checkValidity()) {
+//         event.preventDefault()
+//         event.stopPropagation()
+//       }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+//       form.classList.add('was-validated')
+//     }, false)
+//   })
+// })()
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: listing.geometry.coordinates,
+    zoom: 9,
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h4>${listing.title}</h4><p>Exact location provided after booking</p>`
+        )
+    )
+    .addTo(map);
